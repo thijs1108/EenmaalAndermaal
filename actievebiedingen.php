6a3023f889ef7d0886d836f428f6562eb843409a
WH95 <wouter1995@live.nl>
2016-05-20 09:27:07 +0200

    Merge

--- actievebiedingen.php
+++ actievebiedingen.php
@@ -23,52 +23,7 @@
                 <?php include 'includes/functions.php';?>
                     <div class="content">
                         <div class="large-3 columns">
-                            <ul class="vertical accordion-menu menu" data-accordion-menu>
-                                <li>
-                                    <a href="#"class="subitem">Auto's boten en motoren(3)</a>
-                                    <ul class="menu vertical sublevel-1">
-                                        <li>
-                                            <a href="#"class="subitem">Auto's (3)</a>
-                                            <ul class="menu vertical sublevel-2">
-                                                <li><a class="subitem" href="#">Suv's</a></li>
-                                                <li><a class="subitem" href="#">Sportwagens</a></li>
-                                                <li><a class="subitem" href="#">Hatchback</a></li>
-                                            </ul>
-                                        </li>
-                                        <li><a class="subitem" href="#">Boten</a></li>
-                                        <li><a class="subitem" href="#">Motoren</a></li>
-                                    </ul>
-                                </li>
-                                <li>
-                                    <a href="#"class="subitem">Baby (2)</a>
-                                    <ul class="menu vertical sublevel-1">
-                                        <li><a class="subitem" href="#">Speelgoed</a></li>
-                                        <li><a class="subitem" href="#">Meubels</a></li>
-                                    </ul>
-                                </li>
-                                <li>
-                                    <a href="#"class="subitem">Muziek en Instrumenten (2)</a>
-                                    <ul class="menu vertical sublevel-1">
-                                        <li><a class="subitem" href="#">Muziekblad</a></li>
-                                        <li><a class="subitem" href="#">Instrumenten</a></li>
-                                    </ul>
-                                </li>
-                                <li>
-                                    <a href="#"class="subitem">Meubels (2)</a>
-                                    <ul class="menu vertical sublevel-1">
-                                        <li><a class="subitem" href="#">Banken</a></li>
-                                        <li><a class="subitem" href="#">Stoelen</a></li>
-                                    </ul>
-                                </li>
-                                <li>
-                                    <a href="#"class="subitem">Elektronica (3)</a>
-                                    <ul class="menu vertical sublevel-1">
-                                        <li><a class="subitem" href="#">Computers</a></li>
-                                        <li><a class="subitem" href="#">Telefoon</a></li>
-                                        <li><a class="subitem" href="#">Beamers</a></li>
-                                    </ul>
-                                </li>
-                            </ul>
+                            
 
                         </div>
                         <div class="large-9 columns">
