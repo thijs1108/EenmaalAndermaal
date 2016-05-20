34e32cf63dfc7a1055b6dd523482a3f9b3e3f9c7
WH95 <wouter1995@live.nl>
2016-05-20 08:54:40 +0200

    Update Voortgangs presentatie
    Het categorie menu krijgt nu de oranje kleur on hover,
    Er zitten tussen de plaatjes en de tekst nu een wit ruimte

--- actievebiedingen.php
+++ actievebiedingen.php
@@ -25,10 +25,10 @@
                         <div class="large-3 columns">
                             <ul class="vertical accordion-menu menu" data-accordion-menu>
                                 <li>
-                                    <a href="#">Auto's boten en motoren(3)</a>
+                                    <a href="#"class="subitem">Auto's boten en motoren(3)</a>
                                     <ul class="menu vertical sublevel-1">
                                         <li>
-                                            <a href="#">Auto's (3)</a>
+                                            <a href="#"class="subitem">Auto's (3)</a>
                                             <ul class="menu vertical sublevel-2">
                                                 <li><a class="subitem" href="#">Suv's</a></li>
                                                 <li><a class="subitem" href="#">Sportwagens</a></li>
@@ -40,28 +40,28 @@
                                     </ul>
                                 </li>
                                 <li>
-                                    <a href="#">Baby (2)</a>
+                                    <a href="#"class="subitem">Baby (2)</a>
                                     <ul class="menu vertical sublevel-1">
                                         <li><a class="subitem" href="#">Speelgoed</a></li>
                                         <li><a class="subitem" href="#">Meubels</a></li>
                                     </ul>
                                 </li>
                                 <li>
-                                    <a href="#">Muziek en Instrumenten (2)</a>
+                                    <a href="#"class="subitem">Muziek en Instrumenten (2)</a>
                                     <ul class="menu vertical sublevel-1">
                                         <li><a class="subitem" href="#">Muziekblad</a></li>
                                         <li><a class="subitem" href="#">Instrumenten</a></li>
                                     </ul>
                                 </li>
                                 <li>
-                                    <a href="#">Meubels (2)</a>
+                                    <a href="#"class="subitem">Meubels (2)</a>
                                     <ul class="menu vertical sublevel-1">
                                         <li><a class="subitem" href="#">Banken</a></li>
                                         <li><a class="subitem" href="#">Stoelen</a></li>
                                     </ul>
                                 </li>
                                 <li>
-                                    <a href="#">Elektronica (3)</a>
+                                    <a href="#"class="subitem">Elektronica (3)</a>
                                     <ul class="menu vertical sublevel-1">
                                         <li><a class="subitem" href="#">Computers</a></li>
                                         <li><a class="subitem" href="#">Telefoon</a></li>
