767beaea990c72a58ec0512e990a529b2ed1df3e
thijs1108 <t.beltman@hotmail.nl>
2016-05-20 12:00:30 +0200

    zoekterm verwerken

--- actievebiedingen.php
+++ actievebiedingen.php
@@ -23,41 +23,56 @@
                 <?php include 'includes/functions.php';?>
                     <div class="content">
                         <div class="large-3 columns">
-                                <?php include 'includes/categorie.php'?>
+                            <?php include 'includes/categorie.php'?>
                         </div>
                         <div class="large-9 columns">
-                            <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+                            <form method="get" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+                                <?php
+                                    if(isset($_GET['zoeken']) && !is_null($_GET['zoeken'])){
+                                        $zoekterm = $_GET['zoeken'];
+                                    }
+                                ?>
                                 <input type="submit" value="Zoeken" class="zoekenknop">
                                 <div class="zoeken">
-                                    <input type="text" name="zoeken" value="" placeholder="&#xf002; Zoekterm toevoegen" class="fontawesome zoeken">
+                                    <input type="text" name="zoeken" value="<?php if(isset($zoekterm)){echo $zoekterm;} ?>" placeholder="&#xf002; Zoekterm toevoegen" class="fontawesome zoeken">
                                 </div>
                             </form>
                             <br/>
                             <?php
-                                    $sql = "SELECT titel,voorwerpnummer, max(Bodbedrag)as maxbedrag, COUNT(Bodbedrag)as geboden,looptijdeindeDag, looptijdeindeTijdstip FROM Voorwerp LEFT OUTER JOIN Bod ON Voorwerp.voorwerpnummer=bod.Voorwerp GROUP BY titel,voorwerpnummer, looptijdeindeDag, looptijdeindeTijdstip";
-                                    $result = sqlsrv_query($db, $sql);
-
-                                    while($record=sqlsrv_fetch_array($result))
-                                    {   
-                                        echo '<div class="large-6 columns">';
-                                        echo '<a href="productdetails.php?id='.$record['voorwerpnummer'].'" >';
-                                        echo '<div class="product">';
-                                        $_POST['id'] = $record['voorwerpnummer'];
-                                        echo '<img src="voorwerpen/bieding_'.$record['voorwerpnummer'].'_01.png" alt="'.$record['titel'].'" class="prdimg">'."<br>";
-                                        echo '<b>'.$record['titel'].'</b>';
-                                        echo '<br/>';
-                                        echo 'Hoogste bod: € '.number_format($record['maxbedrag'],2);
-                                        echo '<br/>';  
-                                        echo 'Totaal aantal biedingen:'.$record['geboden'];
-                                        echo '<br/>';
-                                        echo 'Tijd tot sluiting:';
-                                        $date = date_format($record['looptijdeindeDag'], 'Y-m-d');
-                                        echo '<div class="alt-2 right">'.$date.'</div>';
-                                        echo '</div>';
-                                        echo '</a>';
-                                        echo '</div>';
-                                    }
-                                ?>
+                            if (isset($zoekterm)){
+                                $sql = "SELECT titel,voorwerpnummer, max(Bodbedrag)as maxbedrag, COUNT(Bodbedrag)as geboden,looptijdeindeDag, looptijdeindeTijdstip FROM Voorwerp LEFT OUTER JOIN Bod ON Voorwerp.voorwerpnummer=bod.Voorwerp WHERE titel LIKE '%$zoekterm%' GROUP BY titel,voorwerpnummer, looptijdeindeDag, looptijdeindeTijdstip";
+                                $result = sqlsrv_query($db, $sql);
+                            }
+                            else{
+                                $sql = "SELECT titel,voorwerpnummer, max(Bodbedrag)as maxbedrag, COUNT(Bodbedrag)as geboden,looptijdeindeDag, looptijdeindeTijdstip FROM Voorwerp LEFT OUTER JOIN Bod ON Voorwerp.voorwerpnummer=bod.Voorwerp GROUP BY titel,voorwerpnummer, looptijdeindeDag, looptijdeindeTijdstip";
+                                $result = sqlsrv_query($db, $sql);
+                            }
+                            $count = 0;
+                            while($record=sqlsrv_fetch_array($result))
+                            {
+                                $count++;
+                                echo '<div class="large-6 columns">';
+                                echo '<a href="productdetails.php?id='.$record['voorwerpnummer'].'" >';
+                                echo '<div class="product">';
+                                $_POST['id'] = $record['voorwerpnummer'];
+                                echo '<img src="voorwerpen/bieding_'.$record['voorwerpnummer'].'_01.png" alt="'.$record['titel'].'" class="prdimg">'."<br>";
+                                echo '<b>'.$record['titel'].'</b>';
+                                echo '<br/>';
+                                echo 'Hoogste bod: € '.number_format($record['maxbedrag'],2);
+                                echo '<br/>';
+                                echo 'Totaal aantal biedingen:'.$record['geboden'];
+                                echo '<br/>';
+                                echo 'Tijd tot sluiting:';
+                                $date = date_format($record['looptijdeindeDag'], 'Y-m-d');
+                                echo '<div class="alt-2 right">'.$date.'</div>';
+                                echo '</div>';
+                                echo '</a>';
+                                echo '</div>';
+                            }
+                            if($count==0){
+                                echo"Geen resultaten gevonden op de zoekterm: '" . $zoekterm . "'";
+                            }
+                            ?>
                         </div>
 
                         <div class="row">
@@ -84,7 +99,7 @@
     <script src="js/jquery.min.js"></script>
     <script src="js/jquery.countdown.js"></script>
     <script>
-        window.jQuery(function($) {
+        window.jQuery(function ($) {
             "use strict";
 
             $('time').countDown({
@@ -98,7 +113,6 @@
             });
 
         });
-
     </script>
 </body>
 
