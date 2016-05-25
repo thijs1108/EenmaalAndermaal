9b4f8c579a2e68b408f7e18730fd890e621138c7
MFJLBeuzel <maartenbeuzel@gmail.com>
2016-05-25 15:29:16 +0200

    Foot(opnieuw)
    commit

--- footer.php
+++ footer.php
@@ -1,3 +1,8 @@
+<?php
+    define("TITLE", "Home | Eenmaal Andermaal: voor al uw veilingen");
+    //constant named 'Title'
+?>
+
 <!doctype html>
 <html class="no-js" lang="en" dir="ltr">
 
@@ -5,36 +10,27 @@
     <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Eenmaal Andermaal</title>
+    <title><?php echo TITLE; ?></title>
     <link href='https://fonts.googleapis.com/css?family=Roboto' rel='stylesheet' type='text/css'>
     <link rel="stylesheet" href="css/foundation.css">
     <link rel="stylesheet" href="css/app.css">
     <link href="http://cdnjs.cloudflare.com/ajax/libs/foundicons/3.0.0/foundation-icons.css" rel="stylesheet">
-</head>
-
-<body>
     <!-- For Foundation Icons, put this in your head -->
-    <link href="http://cdnjs.cloudflare.com/ajax/libs/foundicons/3.0.0/foundation-icons.css" rel="stylesheet">
-
+</head>
+<body>
     <!-- Footer -->
     <footer class="footer">
         <div class="row full-width">
-            <div class="large-4 columns">
+            <div class="large-9 columns">
                 <i class="fi-laptop"></i>
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum maiores alias ea sunt facilis impedit fuga dignissimos illo quaerat iure in nobis id quos, eaque nostrum! Unde, voluptates suscipit repudiandae!</p>
             </div>
-            <div class="large-4 columns">
-                <i class="fi-html5"></i>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit impedit consequuntur at! Amet sed itaque nostrum, distinctio eveniet odio, id ipsam fuga quam minima cumque nobis veniam voluptates deserunt!</p>
-            </div>
-            <div class="large-2 columns">
-                
-                
-            </div>
-            <div class="large-2 columns">
-                
-                
-            </div>
-        </div>
+            <div class="large-3 columns">
+            <h4>Volg ons</h4>
+            <ul class="footer-links">
+            <li><a href="http://www.twitter.com/VeilingsiteEA"><i class="fi-social-twitter"></i></a></li>
+          </ul>
+          </div>
+
     </footer>
 </body>
