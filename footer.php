b932b3db8b0a0194120d100a0fb239645dac128e
WH95 <wouter1995@live.nl>
2016-05-25 14:59:34 +0200

    Footer Update

--- footer.php
+++ footer.php
@@ -11,36 +11,30 @@
     <link rel="stylesheet" href="css/app.css">
     <link href="http://cdnjs.cloudflare.com/ajax/libs/foundicons/3.0.0/foundation-icons.css" rel="stylesheet">
 </head>
+
 <body>
-<footer class="footer">
-  <div class="row">
-    <div class="small-12 medium-6 large-5 columns">
-      <p class="logo"><i class="fi-shield"></i> EenmaalAndermaal</p>
-      <p class="footer-links">
-        <a href="#">Home</a>
-        <a href="#">Blog</a>
-        <a href="#">Pricing</a>
-        <a href="#">About</a>
-        <a href="#">Faq</a>
-        <a href="#">Contact</a>
-      </p>
-      <p class="copywrite"><?php
-      echo "<p>Copyright &copy; 2016-" . date("Y") . " EenmaalAndermaal </p>";
-      ?></p>
-    </div>
-    <div class="small-12 medium-6 large-4 columns">
-      <ul class="contact">
-        <li><p><i class="fi-marker"></i>Ruitenberglaan 26, Arnhem</p></li>
-        <li><p><i class="fi-mail"></i>contact@eenmaalandermaal.com</p></li>
-      </ul>
-    </div>
-    <div class="small-12 medium-12 large-3 columns">
-      <p class="about">EenmaalAndermaal</p>
-      <p class="about subheader">Voor al uw veilingen</p>
-      <ul class="inline-list social">
-        <a href="http://www.twitter.com/VeilingsiteEA"><i class="fi-social-twitter"></i></a>
-      </ul>
-    </div>
-  </div>
-</footer>
+    <!-- For Foundation Icons, put this in your head -->
+    <link href="http://cdnjs.cloudflare.com/ajax/libs/foundicons/3.0.0/foundation-icons.css" rel="stylesheet">
+
+    <!-- Footer -->
+    <footer class="footer">
+        <div class="row full-width">
+            <div class="large-4 columns">
+                <i class="fi-laptop"></i>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum maiores alias ea sunt facilis impedit fuga dignissimos illo quaerat iure in nobis id quos, eaque nostrum! Unde, voluptates suscipit repudiandae!</p>
+            </div>
+            <div class="large-4 columns">
+                <i class="fi-html5"></i>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit impedit consequuntur at! Amet sed itaque nostrum, distinctio eveniet odio, id ipsam fuga quam minima cumque nobis veniam voluptates deserunt!</p>
+            </div>
+            <div class="large-2 columns">
+                
+                
+            </div>
+            <div class="large-2 columns">
+                
+                
+            </div>
+        </div>
+    </footer>
 </body>
